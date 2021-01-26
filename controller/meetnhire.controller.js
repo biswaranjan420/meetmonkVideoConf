9fa4b5ce0bfed3afd050c0385db77e4229d69763
biswaranjan420 <55794791+biswaranjan420@users.noreply.github.com>
2021-01-27 01:55:44 +0530

    test

--- controller/meetnhire.controller.js
+++ controller/meetnhire.controller.js
@@ -6,7 +6,7 @@ exports.joinRoom = (req, res) => {
         if (roomName && roomName != '') {
             const url = 'https://core.meetmonk.com/#/' + roomName;
             res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.write('<html><body><iframe name="meetNhireIframe" style="position:fixed; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;" src="' + url + '" allow="microphone; camera" title="Classroom"></iframe></body></html>');
+            res.write('<html><head><meta http-equiv="refresh"  content="0; url=' + url + '"/ ></head><body></body></html>');
             res.end();
         }
 
